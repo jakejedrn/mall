d7e2c0a6949535e6823e76856fbcf9ae37b1ca39
unknown <15684199833@163.com>
2020-11-23 14:45:09 +0800

    初始化项目

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,36 @@
 <template>
-  
+  <div id="home">
+    <nav-bar class="home-nav">
+      
+      <!-- <div slot="left">购物街</div> -->
+      <div slot="center">购物街</div>
+      <!-- <div slot="right">购物街</div> -->
+      
+    </nav-bar>
     <h2>Home</h2>
+    
+  </div>
+    
 
 </template>
 
 <script>
-export default {
-  name: '',
-  data () {
-    return {
 
-    };
-  },
-  components: {},
-  mounted () {},
-  methods: {}
-}
+  import NavBar from '../../components/common/navbar/NavBar'
+
+  export default {
+    name: 'Home',
+    data () {
+      return {
+
+      };
+    },
+    components: {
+      NavBar
+    },
+    mounted () {},
+    methods: {}
+  }
 </script>
 
 <style scoped>
